023eff4d7d9dff9cbfdfcba4f83cba973660d4fe
Anish Agarwal <anishagarwal.sg@gmail.com>
2026-01-22 19:44:58 +0800

    Initial Commit

---
+++ Vault/VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaultApp.swift
+//  Vault
+//
+//  Created by Anish Agarwal on 22/1/26.
+//
+
+import SwiftUI
+
+@main
+struct VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
